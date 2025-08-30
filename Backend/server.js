ce405886c5d7d0115332eb2cb15146a5627ac93f
Devoloper-Deep123 <dasd84813@gmail.com>
2025-08-30 15:34:30 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ import route2 from "./Routers/ServiceRouter.js"
 
 app.use(express.json());
 app.use(cors({
-  origin: ["https://backend-project-ivory.iota.vercel.app","http://localhost:5173"],
+  origin: [process.env.FRONTEND_URL,"http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
